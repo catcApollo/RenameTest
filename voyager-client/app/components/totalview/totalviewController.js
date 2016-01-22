d84564e7e0f32b7d527e615e4914cd4036e817cc
SDS <SDS@172.16.0.138>
2016-01-22 10:35:24 +0900

    init

--- voyager-client/app/components/totalview/totalviewController.js
+++ voyager-client/app/components/totalview/totalviewController.js
@@ -16,6 +16,17 @@ VoyagerApp.config(['$stateProvider',
 VoyagerApp.controller('totalviewController', 
 		['$scope',
 						function totalviewController($scope) {
-							$scope.dataList=[{"name" : "KIM SANG WOO"}
-							, {"name" : "LEE SEUNG HEE"}]
-							} ]);
+							$scope.dataList=[{"name" : "KIM SANG WOO", "today": "하하하하하하하하!"}
+							, {"name" : "LEE SEUNG HEE", "today": "다들 커피 쏘세요!"}
+							, {"name" : "LEE DONG SEOK", "today": "오늘도 야근!"}
+							, {"name" : "SHIN NA EUN", "today": "불금!"}
+							, {"name" : "JUNG SANG UP", "today": "자전거로 출퇴근해야지!"}
+							, {"name" : "MIN HACK JUN", "today": "이놈의 회의!"}
+							, {"name" : "PARK HYUN JEONG", "today": "이놈의 회의 (2) !"}
+							, {"name" : "CONAN", "today": "한우 먹어야지!"}
+							, {"name" : "RANI", "today": "버츠비 짱짱!"}
+							, {"name" : "JIMMY", "today": "감기 빠잉!"}							
+							];
+						}
+		]						
+);
